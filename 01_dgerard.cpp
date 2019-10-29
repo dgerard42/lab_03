9ca9e1e9ccdb233b7fa1e2756574dd3b36b3b77b
dgerard42 <daniella4422@gmail.com>
2019-10-29 12:27:33 -0700

    ejejejejejejeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

---
+++ 01_dgerard.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                  .--.                      */
+/*   01_dgerard.cpp                              __/ o  ".                    */
+/*                                              `  ),    "-.                  */
+/*   By: dany <github.com/dgerard42>               |;;,      "-._             */
+/*                                                 ';;;,,    ",_ "=-._        */
+/*   Created: 2019/10/29 11:58:39 by dany            ':;;;;,,..-``"-._`"-.    */
+/*   Updated: 2019/10/29 12:22:33 by dany              _/_/`           `'"`   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+unsigned int        getPlace(unsigned int number){
+
+    int     place
+    while (number > 0){
+        
+    }
+}
+
+unsigned int        reverse(unsigned int number){
+
+    if (number <= 0)
+        return;
+    else {
+        return (number % 10) * getPlace() + reverse(number / 10)   
+    }
+}
