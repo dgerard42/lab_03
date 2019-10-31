db9abc718e7885336b078cc6c44f14200026d8b9
dgerard42 <daniella4422@gmail.com>
2019-10-31 12:13:14 -0700

    lil bit of lab work

--- 01_dgerard.cpp
+++ 01_dgerard.cpp
@@ -6,28 +6,43 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/29 11:58:39 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/29 12:22:33 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/10/31 12:01:27 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
+const int   BASE = 10;
+
 unsigned int        getPlace(unsigned int number){
 
-    int     place
+    int     place = 0;
+
     while (number > 0){
-        
+        place++;
+        number /= BASE;
     }
+    return place;
 }
 
 unsigned int        reverse(unsigned int number){
 
-    if (number <= 0)
-        return;
+    int place = getPlace(number);
+        
+    if (number == 0)
+        return 0;
     else {
-        return (number % 10) * getPlace() + reverse(number / 10)   
+        return ((number % BASE) * (pow(BASE, (getPlace(number) - 1))) + reverse(number / BASE));
     }
+}
+
+int                 main(){
+
+    cout << reverse(123) << endl;
+    cout << reverse(1234567890) << endl;
+    cout << reverse(1230303) << endl;
 }
---
+++ 02_dgerard.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                  .--.                      */
+/*   02_dgerard.cpp                              __/ o  ".                    */
+/*                                              `  ),    "-.                  */
+/*   By: dany <github.com/dgerard42>               |;;,      "-._             */
+/*                                                 ';;;,,    ",_ "=-._        */
+/*   Created: 2019/10/31 12:01:45 by dany            ':;;;;,,..-``"-._`"-.    */
+/*   Updated: 2019/10/31 12:12:58 by dany              _/_/`           `'"`   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int     binarySearch(int sortedArray[], int value, int start, int end){
+
+
+
+}
