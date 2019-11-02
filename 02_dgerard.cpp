23b678595fdf8657b872db371e1cb1a9e1558ffb
dgerard42 <daniella4422@gmail.com>
2019-11-02 01:27:41 -0700

    why did i have the urgent need to program at 1AM?? why is my brain like this? anyways, heres binary search. and yeah, it was easier recursively. recursion is good, actually

--- 02_dgerard.cpp
+++ 02_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/10/31 12:01:45 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/10/31 12:12:58 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/11/02 01:26:06 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,22 @@ using namespace std;
 
 int     binarySearch(int sortedArray[], int value, int start, int end){
 
+    int     index = end - ((end - start) / 2);
+    
+    if (sortedArray[index] == value)
+        return index;
+    else if (start == end)
+        return -1;
+    else if (sortedArray[index] > value)
+        binarySearch(sortedArray, value, start, index);
+    else if (sortedArray[index] < value)
+        binarySearch(sortedArray, value, index, end);
+}
 
+int     main(){
 
+    int arr[10] = {0,1,2,3,4,5,6,7,8,9};
+        
+    cout << binarySearch(arr, 3, 0, 9) << endl;
+    return 0;
 }
