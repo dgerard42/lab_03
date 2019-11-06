f7001cd60a9ed6811cef93563ef685718dee35ae
dgerard42 <daniella4422@gmail.com>
2019-11-05 23:09:28 -0800

    yikes im behind

--- 03_dgerard.cpp
+++ 03_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/11/03 22:21:34 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/11/03 23:08:40 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/11/04 18:05:51 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,30 @@ using namespace std;
 
 const int   MAX_NAMES = 20;
 
-int         getNames(string names[]){
+void        printNames(string names[], int numNames){
 
+    int     nameIndex = 0;
+
+    while (nameIndex < numNames - 1){
+        cout << names[nameIndex];
+        if (nameIndex != numNames - 2)
+            cout << ", ";
+        nameIndex++;
+    }
+    cout << endl;
+    return;
+}
+
+int         getNames(string names[]){
 
     int     numNames = 0;
 
     cout << "enter the names you wish to be sorted, one at a time. when you are"
         " done, enter the string \"DONE\"" << endl;
     while (numNames < MAX_NAMES && (numNames <= 0 || names[numNames - 1] != "DONE")){
+        cout << "Enter a name: ";
         cin >> names[numNames];
+        cout << endl;
         numNames++;
     }
     return numNames;
@@ -36,8 +51,8 @@ int         main(){
     int     numNames;
     string  names[MAX_NAMES];
 
-    getNames();
-    sort();
-    printNames();
+    numNames = getNames(names);
+    //sort();
+    printNames(names, numNames);
     return 0;
 }
