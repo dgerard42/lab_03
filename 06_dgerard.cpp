9c2cb8f961d520367a96d8067f739f5ca57fa1f8
dgerard42 <daniella4422@gmail.com>
2019-11-13 08:21:12 -0800

    structuring out the file searcher

--- 06_dgerard.cpp
+++ 06_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/11/08 20:10:35 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/11/11 21:16:12 by dany              _/_/`           `'"`   */
+/*   Updated: 2019/11/12 23:39:31 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,26 +27,38 @@ const string    INPUT_FILE = "janeEyre.txt";
 const int       LINES = 289;
 const string    OUTPUT_FILE = "output.txt";
 
+int             printInfo(vector<int> &lineNumbers, int numMatches){
 
+    
+}
 
+int             searchLine(string haystack, string needle){
 
+    int         match = 0;
+    int         search = 0;
+    
+    while (haystack[search] != needle[match])
+        search++;
+    while (match + search && haystack[match + search] == needle[match])
+        match++;
+    if (haystack.length == 
+        return  
+    if (needle.length == match)
+        return 1;
+}
+    
 int             main(){
     
     string      currentLine;
-
     ifstream    inputFile;
     inputFile.open(INPUT_FILE);
-
-    ofstream    outputFile;
-    outputFile.open(OUTPUT_FILE);
 
     if (inputFile && outputFile){
         for (int line = 0; line < LINES; line++){
             getline(inputFile, currentLine);
-            outputFile << currentLine << endl;
+            searchLine();
         }
         inputFile.close();
-        outputFile.close();
     } else {
         cout << "this file won't open" << endl;
     }
