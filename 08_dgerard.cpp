07defd9ff4f979bd5ce01a0097cf49d2cc2321f0
dgerard42 <daniella4422@gmail.com>
2019-11-26 13:25:42 -0800

    made it more modular. realized that there is no difference between encryption and decryption :P

--- 08_dgerard.cpp
+++ 08_dgerard.cpp
@@ -6,7 +6,7 @@
 /*   By: dany <github.com/dgerard42>               |;;,      "-._             */
 /*                                                 ';;;,,    ",_ "=-._        */
 /*   Created: 2019/11/08 20:10:35 by dany            ':;;;;,,..-``"-._`"-.    */
-/*   Updated: 2019/11/25 12:12:08 by doot              _/_/`           `'"`   */
+/*   Updated: 2019/11/26 13:17:49 by dany              _/_/`           `'"`   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,47 +24,49 @@ const char      ALPHA_CHARS = 26;
 const char      NUM_CHARS = 10;
 const char      CHAR_0 = 48;
 const char      CHAR_9 = 57;
+const int       ALPHA_ENCRYPTION = 13;
+const int       NUM_ENCRYPTION = 5;
 
-char    rot13(char original, bool encryptionDirection){
+char    rotAlpha(char original, int rotSize){
     
     char    result = original;
-    int     encryption = (encryptionDirection == true) ? 13 : -13;
 
     if (original >= LOWERCASE_A && original <= LOWERCASE_Z){
-        if (original - LOWERCASE_A < ALPHA_CHARS - encryption)
-            result = original + encryption;
+        if (original - LOWERCASE_A < ALPHA_CHARS - rotSize)
+            result = original + rotSize;
         else
-            result = (((original - LOWERCASE_A) + encryption) % ALPHA_CHARS) + LOWERCASE_A;
+            result = (((original - LOWERCASE_A) + rotSize) % ALPHA_CHARS) + LOWERCASE_A;
     } else if (original >= UPPERCASE_A && original <= UPPERCASE_Z){
-        if (original - UPPERCASE_A < ALPHA_CHARS - encryption)
-            result = original + encryption;
+        if (original - UPPERCASE_A < ALPHA_CHARS - rotSize)
+            result = original + rotSize;
         else
-            result = (((original - UPPERCASE_A) + encryption) % ALPHA_CHARS) + UPPERCASE_A;
+            result = (((original - UPPERCASE_A) + rotSize) % ALPHA_CHARS) + UPPERCASE_A;
     }        
     return result;
 }
 
-char    rot5(char original, bool encryptionDirection){
+char    rotNum(char original, int rotSize){
 
     char    result = original;
-    int     encryption = (encryptionDirection == true) ? 5 : -5;
 
     if (original >= CHAR_0 && original <= CHAR_9){
-        if (original - CHAR_0 < NUM_CHARS - encryption)
-            result = original + encryption;
+        if (original - CHAR_0 < NUM_CHARS - rotSize)
+            result = original + rotSize;
         else
-            result = (((original - CHAR_0) + encryption) % CHAR_0) + CHAR_0;
+            result = (((original - CHAR_0) + rotSize) % NUM_CHARS) + CHAR_0;
     }
     return result;
 }
 
-string  translateLine(string line, bool encryptionDirection){
+string  translateLine(string line){
 
     string  result (line.length(), ' ');
 
     for (int index = 0; index < line.length(); index++){
-        result[index] = rot13(line[index], encryptionDirection);
-        result[index] = rot5(line[index], encryptionDirection);
+        if (line[index] >= CHAR_0 && line[index] <= CHAR_9)
+            result[index] = rotNum(line[index], NUM_ENCRYPTION);
+        else
+            result[index] = rotAlpha(line[index], ALPHA_ENCRYPTION);
     }
     return result;
 }
@@ -79,25 +81,6 @@ string  getUserInput(string userMessage){
     return userInput; 
 }
 
-int    askDirection(){
-
-    int     encryptionDirection;
-    string  userInput;
-
-    cout << "would you like to encrypt or decrypt the file?";
-    cout << " type \"e\" for encryption, or \"d\" for decryption:" << endl;
-    cin >> userInput;
-    if (userInput == "e")
-        encryptionDirection = 1;
-    else if (userInput == "d")
-        encryptionDirection = 0;
-    else {
-        cout << "that was not valid input. please try again." << endl;
-        encryptionDirection = -1;
-    }
-    return encryptionDirection;
-}
-
 int     main(){
 
     ifstream    inputFile;
@@ -105,15 +88,14 @@ int     main(){
     string      currentLine;
     string      inputRequest = getUserInput("enter the name of the file you wish to change");
     string      outputRequest = getUserInput("enter the name of the file where you would like to store the results");
-    int         encryptionDirection = askDirection();
+//    string inputRequest = "output.txt";
+//    string outputRequest = "empty.txt";
     
     inputFile.open(inputRequest);
     outputFile.open(outputRequest);
-    if (encryptionDirection == -1)
-        cout << "exiting program now" << endl;
-    else if (inputFile && outputFile){
+    if (inputFile && outputFile){
         while (getline(inputFile, currentLine)){
-           currentLine = translateLine(currentLine, encryptionDirection);
+           currentLine = translateLine(currentLine);
            outputFile << currentLine << endl;
         }
         inputFile.close();
